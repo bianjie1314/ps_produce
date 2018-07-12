b81726a9224a3c8279070a65010af90d7494c412
xiangzheng@ziguangcn.com <xiang_zheng@ziguangcn.com>
2018-07-12 14:05:41 +0800

    页面

--- src/main/webapp/resources/js/produce/order.js
+++ src/main/webapp/resources/js/produce/order.js
@@ -33,20 +33,66 @@ function queryOrderName(){
 	});
 }
 function add() {
-	var $f = $('<div role="form"></div>');
-    $f.append('<div class="input-prepend input-group">'+
+    var $f = $('<div role="form"></div>');
+    $f.append('<div class="form-group" style="padding: 10px 50px;"><div class="input-group"><label class="input-group-addon">订单编号</label><input type="text"   name="time" id="number" class="form-control"/></div></div>');
+/*    $f.append('<div class="form-group"><button type="button" class="btn btn-primary">添加订单</button></div>');
+*/
+  /*  $f.append('<div class="row">'+
+    		'<div class="input-prepend input-group">'+
     		'<span class="add-on input-group-addon">订单编号：</span>'+
     		'<input type="text"  style="width: 230px" name="time" id="number" class="form-control"/> '+
-    		'<button type="button" class="btn btn-primary">添加订单</button>'+
-    		'<button type="button" class="btn btn-default">取消</button>'+
+    		
+    		'</div>'+
     		'</div>');
-   /* $f.append('<div class="col-sm-6">'+
+   $f.append('<div class="col-sm-6">'+
     		'<button type="button" class="btn btn-primary">添加订单</button>'+
     		'<button type="button" class="btn btn-default">取消</button>'+
     		'</div>');*/
 	   $f.dialog({
+		   title : "添加订单",
+		  
         onClose : function() {
             $(this).dialog("destroy");
+        }, buttons : {
+            "添加订单" : function() {
+            	
+         	   if(!$.validate($f))
+        	   		return false;
+                var type =  $f.find('[dialog-id=type]').val();
+                var label = $f.find('[dialog-id=label]').val();
+                var value =  $f.find('[dialog-id=value]').val();
+                var description =  $f.find('[dialog-id=description]').val();                  
+                var sort = $f.find('[dialog-id=sort]').val();
+                var param = {
+             	   type : type,
+                    label : label,
+                    value : value,
+                    description : description,
+                    sort : sort,
+                    id : data.id
+                };
+                $.ajax({
+                    url : ctx + "/system/dict/update",
+                    type : "POST",
+                    contentType : 'application/json; charset=utf-8',
+                    dataType : 'json',
+                    data : JSON.stringify(param),
+                    success : function(result) {
+                        if (result.ret == 0) {
+                            table.ajax.reload();
+                            $.messager.popup("修改成功!");
+                        } else {
+                        	$.messager.popup("修改失败!");
+                        }
+                    },
+                    complete : function() {
+                        $f.dialog("destroy");
+                    }
+                });
+                
+            }, "取消" : function() {
+            	 $(this).dialog("destroy");
+            }
         }
     });
 }
